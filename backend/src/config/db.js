70aaedd0a2f15853b423bcc94186a0416b89350d
D.M.C Dissanayake <132731661+Chamindu-DM@users.noreply.github.com>
2025-10-09 23:44:57 +0530

    feat: add initial backend setup with Express, PostgreSQL integration, and environment configuration

---
+++ backend/src/config/db.js
@@ -0,0 +1,24 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+if (!process.env.DB_USER || !process.env.DB_HOST || !process.env.DB_DATABASE || !process.env.DB_PASSWORD || !process.env.DB_DBPORT) {
+    throw new Error("Missing required environment variables for database connection.");
+}
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_DBPORT,
+});
+
+pool.on("connect", () =>{
+    console.log("Connection pool established with Database!");
+});
+
+export default pool;
